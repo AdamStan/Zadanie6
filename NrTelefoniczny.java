3fe7966a50c788350873b3f254018f83aa5abf13
Adam <adam15stan@gmail.com>
2017-09-18 15:49:21 +0200

    Ostatecznie, reszta jest dobrze

--- NrTelefoniczny.java
+++ NrTelefoniczny.java
@@ -1,7 +1,7 @@
 package zadanie6;
 public class NrTelefoniczny implements Comparable<NrTelefoniczny> {
-    Integer nrkierunkowy;
-    Integer nrTelefonu;
+    private Integer nrkierunkowy;
+    private Integer nrTelefonu;
     /*Methods*/
     public NrTelefoniczny(){
         nrkierunkowy = 0;
@@ -18,16 +18,25 @@ public class NrTelefoniczny implements Comparable<NrTelefoniczny> {
     public String toString(){
         return String.format("%d %d", nrkierunkowy, nrTelefonu);
     }
-    public int compareTo(NrTelefoniczny other){
+    public int compareTo(NrTelefoniczny other){ //pozwala porownywac obiekty
         boolean last_nrt = this.nrTelefonu.equals(other.nrTelefonu);
         boolean last_nrk = this.nrkierunkowy.equals(other.nrkierunkowy);
         if((last_nrt == true) && (last_nrk == true)) return 0;
+        else if(last_nrk == true || last_nrt == true) return -1;
         else return 1;
     }
     public int getNrkierunkowy(){
+        Integer buff = new Integer(nrkierunkowy);
+        /* Integer jest klasą, zwracana jest referencja do obiektu
+        kiedy mamy:
         return nrkierunkowy;
+        to zwracamy referencje do obiektu (nie wartość)
+        dlatego musisz utworzyc nowy obiekt, nie bedzie mozliwosci
+        modyfikowania wartosci prywatnej dzieki temu*/
+        return buff;
     }
     public long getNrTelefonu(){
-        return nrTelefonu;
+        Integer buff = new Integer(nrTelefonu);
+        return buff;
     }
 }
